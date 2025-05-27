8a324966b8961a62450cfc95715f708999b33af7
dhkim0124 <dhkim92@icloud.com>
2025-05-27 19:04:41 +0900

    feat(ui): 대시보드 사이드바 추가

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+import { AppSidebar } from "./_components/app-sidebar";
+
+import { SidebarProvider } from "@/components/ui/sidebar";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      {children}
+    </SidebarProvider>
+  );
+}
