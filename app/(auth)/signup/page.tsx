0e65213e0f2f7f367a159d8e6110d0265ff79dc8
pyhilandjy <hilandjy@gmail.com>
2025-05-29 17:18:12 +0900

    feat: 회원가입 페이지에 이메일 인증 및 비밀번호 설정 폼 추가

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,16 +1,19 @@
+"use client";
+import { useState } from "react";
 import { SignUpForm } from "./_components/signup-form";
-// import { EmailVerificationForm } from "./_components/email-verification-form";
-// import { PasswordSetupForm } from "./_components/password-setup-form";
+import { EmailVerificationForm } from "./_components/email-verification-form";
+import { PasswordSetupForm } from "./_components/password-setup-form";
 
 export default function SignUpPage() {
+  const [email, setEmail] = useState("");
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <div className="space-y-8">
-          <SignUpForm />
-          {/* 나중에 작업할때 확인할것 */}
-          {/* <EmailVerificationForm />
-          <PasswordSetupForm /> */}
+          <SignUpForm setEmail={setEmail} />
+          <EmailVerificationForm email={email} />
+          <PasswordSetupForm />
         </div>
       </div>
     </div>
