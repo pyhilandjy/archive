87a1832cfbd107254aa7e95dceef38ef01040582
pyhilandjy <hilandjy@gmail.com>
2025-07-01 16:15:24 +0900

    feat: useWebSocket 훅 추가하여 WebSocket 연결 및 메시지 처리 기능 구현

---
+++ hooks/use-websocket.ts
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import {
+  connectWebSocket,
+  disconnectWebSocket,
+  onMessage,
+} from "@/lib/websocket-client";
+import { useQueryClient } from "@tanstack/react-query";
+
+export const useWebSocket = (categoryId?: string) => {
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    connectWebSocket();
+
+    const unsubscribe = onMessage((data: Record<string, unknown>) => {
+      if (data.type === "status_update") {
+        queryClient.invalidateQueries({
+          queryKey: ["contents", categoryId ?? null],
+        });
+      }
+    });
+
+    return () => {
+      unsubscribe();
+      // disconnectWebSocket();
+    };
+  }, [categoryId, queryClient]);
+};
